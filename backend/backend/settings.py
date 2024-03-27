c4f0f5b67ecd370147bff9db9bb9d4f757317960
Ole <105752308+Banquished@users.noreply.github.com>
2024-03-27 23:06:28 +0100

    Backend v1.0 finished

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
